187dc2ca5d1ca36f2c633f27b39c0bf3c67f9a87
rogerdahl <git@dahlsys.com>
2022-08-04 12:59:38 -0600

    Add support for GitHub Flavored Markdown (gfm)

--- webapp/eml_text_type.py
+++ webapp/eml_text_type.py
@@ -8,6 +8,7 @@ import daiquiri
 import lxml.etree
 import lxml.objectify
 import markdown
+import grip
 
 log = daiquiri.getLogger(__name__)
 
@@ -61,18 +62,39 @@ def _find_immediate_children(text_type_el: lxml.etree.Element) -> [lxml.etree.El
     return text_type_el.xpath(f'*')
 
 
-def _markdown_to_html(markdown_el: lxml.etree.Element):
+def _markdown_to_html(markdown_el: lxml.etree.Element, force_local: bool = False):
+    """Return the contents of a markdown element as HTML.
+
+    The EML spec specifies GitHub Flavored Markdown (gfm), and there are no local
+    gfm processors for Python. So we send the markdown to GitHub for processing.
+
+    Notes:
+        - The markdown processor in the Python standard library can handle ALMOST all
+          markdown that occurs in EML documents, so we might want to look into trying to
+          detect if we need to send the markdown to GitHub.
+        - GitHub has rate limiting on the markdown requests. The limit can be increased
+          by connecting with an account instead of anonymously. We currently connect
+          anonymously.
+        - If GitHub markdown processing fails due to rate limiting or other issues, we
+          fall back to processing the markdown locally with the standard Python markdown
+          processor.
+        - The standard Python markdown processor comes with many extensions, and we
+          enable all of these in order to ensure support for as many markdown constructs
+          as possible.
+    """
     markdown_str = markdown_el.xpath('text()')[0]
     dedent_markdown_str = textwrap.dedent(markdown_str)
     log.info(f'Processing as Markdown:\n----\n{dedent_markdown_str}\n----\n')
-    return markdown.markdown(
-        dedent_markdown_str, extensions=DEFAULT_MARKDOWN_EXTENSIONS
-    )
+    log.info(f'Connecting to GitHub for markdown rendering...')
+    try:
+        return grip.render_content(dedent_markdown_str)
+    except Exception as e:
+        log.warn(f'GitHub markdown rendering failed with exception: {str(e)}')
+    log.info(f'Using local markdown processor')
+    return markdown.markdown(dedent_markdown_str, extensions=DEFAULT_MARKDOWN_EXTENSIONS)
 
 
-def _docbook_to_html(
-    docbook_el: lxml.etree.Element, xsl_path: pathlib.Path = XSL_PATH
-) -> str:
+def _docbook_to_html(docbook_el: lxml.etree.Element, xsl_path: pathlib.Path = XSL_PATH) -> str:
     xml_str = get_etree_as_pretty_printed_xml(docbook_el)
     log.info(f'Processing as DocBook hierarchy:\n----\n{xml_str}\n----\n')
     xslt_el = lxml.etree.parse(xsl_path.as_posix())
@@ -112,9 +134,7 @@ def get_etree_as_pretty_printed_xml(el: lxml.etree.Element) -> str:
     """etree to pretty printed XML"""
     # assert isinstance(el, lxml.etree._Element), f'Expected Element. Received {type(el)}'
     if hasattr(el, 'getroottree'):
-        lxml.objectify.deannotate(
-            el.getroottree(), cleanup_namespaces=True, xsi_nil=True
-        )
+        lxml.objectify.deannotate(el.getroottree(), cleanup_namespaces=True, xsi_nil=True)
     return lxml.etree.tostring(
         el, pretty_print=True, with_tail=False, xml_declaration=False
     ).decode('utf-8')
